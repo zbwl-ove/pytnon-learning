8de45b8a785cc93475d9ec3e4b239a1ff566731c
zbwl.ove <zbw.love.ue@gmail.com>
2026-03-19 08:06:03 +0530

    making a password masker

---
+++ libraries/slice/project3.py
@@ -0,0 +1,11 @@
+# password masker
+import sys
+# checkeing some error
+if len(sys.argv)!= 2:
+    sys.exit(" need two arguments : ")
+# limit the visible  part of the password  
+password = sys.argv[1]
+print( password[:3] ,end="")
+# Now printing the rest with "*"
+mask = "*"* max(0, len ( password)- 3)
+print( mask)
