d027d1c49e4d13eebc4a9f2722d4c8140971c778
zbwl.ove <zbw.love.ue@gmail.com>
2026-04-08 12:41:24 +0530

    add 2 file and correct some code

--- file.io/problem3.py
+++ file.io/problem3.py
@@ -2,9 +2,11 @@ import sys
 def get_argv():
     if len( sys.argv)<3:
         sys.exit( "please enter both argumetn's")
-    return sys.argv[1] , sys.argv[2]
-name1 , name2 = get_argv()
-with open ("names.txt" , "w") as f :
-    content =f.write  (f"{name1 }\n{name2}")
+    return sys.argv[1].strip() , sys.argv[2].strip()
+def main ():
     
-            # f.write(f"{name1}\n{name2}")
+    name1 , name2 = get_argv()
+    with open ("names.txt" , "w") as f :
+        f.write  (f"{name1 }\n{name2}")
+if __name__=="__main__":
+    main ()      
