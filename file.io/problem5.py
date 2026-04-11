54983309096144055964ccc394105d3fdfc80844
zbwl.ove <zbw.love.ue@gmail.com>
2026-04-11 07:37:54 +0530

    name's store program

--- file.io/problem5.py
+++ file.io/problem5.py
@@ -1,8 +1,12 @@
-with open ("name.txt" ,"a") as f : 
+# TAKE INPUT AS APPEND MODE SO OLD DATE NOT CHANGE  
+with open ("name.txt" ,"a") as f :
     while True:
-        names = input("enter name's  or tyip exit : ")
-        if names == "exit":
+        names =  input("enter name's  or tyip exit : ")
+# TAKING INPUT AS LOWER 
+        if names.lower() == "exit":
             break
+# BREAK PROGRAM BY TYIP EXIT
         else:
-            content = f .write (names)
+            # TAKE NEXT LINE BY \N WITH  "" 
+            f .write (names  + "\n" )
             print(f"sucsessfully added" , names )
